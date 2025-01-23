22152dd81e7193468b42a44d0f6e61688c17a71b
BibekKumarKushwaha <bibekkumar345@gmail.com>
2025-01-23 14:54:20 +0545

    model

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -1,32 +1,23 @@
-const mongoose = require('mongoose');
+// paymentModel.js
+const mongoose = require("mongoose");
 
 const paymentSchema = new mongoose.Schema(
   {
-    transactionId: { type: String },
-    pidx: { type: String, unique: true },
-    bookings: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'booking',
-        required: true,
-      },
-    ],
-    amount: { type: Number },
-    dataFromVerificationReq: { type: Object },
-    apiQueryFromUser: { type: Object },
-    paymentGateway: {
-      type: String,
-      enum: ['khalti', 'esewa', 'connectIps'],
-      required: true,
-    },
+    transactionId: String,
+    pidx: String,
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: "PurchasedItem" },
+    amount: Number,
+    dataFromVerificationReq: Object,
+    apiQueryFromUser: Object,
+    paymentGateway: { type: String, enum: ["khalti"] },
     status: {
       type: String,
-      enum: ['success', 'pending', 'failed'],
-      default: 'pending',
+      enum: ["success", "pending", "failed"],
+      default: "pending",
     },
     paymentDate: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
-const Payment = mongoose.model('payment', paymentSchema);
-module.exports = Payment;
+
+module.exports = mongoose.model("Payment", paymentSchema);
